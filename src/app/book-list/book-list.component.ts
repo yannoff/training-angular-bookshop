733fad25ab8d6049697d7681cef7c03402d2bb5d
Yannoff <yannoff.it@gmail.com>
2022-01-06 12:07:44 +0100

    [book-list] Refactor books collection handling

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -10,16 +10,15 @@ import { Book } from './book'
 export class BookListComponent implements OnInit, OnChanges, AfterContentInit, OnDestroy {
 
   title:string = "The book list";
-  categories:string[] = [];
+
+  // Category filter possible values
+  categories:string[] = [ "Novels", "IT Books" ];
+
+  // The filtered book collection
   books:Book[] = [];
-  category:string = 'ALL';
 
-  constructor() {
-  }
-
-  load(): void {
-      this.categories = [ "Novels", "IT Books" ];
-      this.books = [
+  // Referential containing all available books
+  all:Book[] = [
         { id: 1, title: "Hacking techniques", author: "Jon Erickson", description: "Dive into the possibilities of any system", category: "IT Books", editor: "Pearson", price: "30 USD", ISBN: "978-2-7440-2264-7", rating: 5 },
         { id: 2, title: "Barney's Version", author: "Mordecai Richler", description: "One of the most entertaining ever written canadian story", category: "Novels", editor: "Gli Adelphi", price: "12 USD", ISBN: "978-88-459-1982-4", rating: 5 },
         { id: 3, title: "Weep no more, my lady", author: "Mary Higgins Clark", description: "", category: "Novels", editor: "Simon & Schuster Inc.", price: "15 USD", ISBN: "2-7242-7350-8" },
@@ -28,6 +27,15 @@ export class BookListComponent implements OnInit, OnChanges, AfterContentInit, O
         { id: 6, title: "The broom of the system", author: "John Foster Wallace", description: "", category: "Novels", editor: "", price: "14 USD", ISBN: "978-88-06-21197-4" },
         { id: 7, title: "A book on C", author: "Al Kelley / Ira Pohl", description: "Programming in C", category: "IT Books", editor: "Benjamin/Cummings", price: "22 USD", ISBN: "0-8053-0060-0", rating: 3.85 },
       ];
+
+  // Default category filter
+  category:string = 'ALL';
+
+  constructor() {
+  }
+
+  load(): void {
+      this.books = this.all;
   }
 
   ngOnInit(): void {
